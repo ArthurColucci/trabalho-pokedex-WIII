605769ff40354362c3f12cd1c373cb43abc690ab
arthu <arthu@NTBARTR5>
2023-04-24 19:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ pokedex/Models/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace pokedex.Models
+{
+
+    public class PokemonList
+    {
+        public List<Pokemon> Pokemons { get; set; } = new List<Pokemon>();
+        public int? Page { get; set; }
+    }
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
